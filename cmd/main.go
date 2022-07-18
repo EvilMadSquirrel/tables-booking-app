11db96dc31814d0b2125d2be6eb58e601a8288af
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-07-18 08:57:11 +0300

    added config and Makefile

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	tblBkng "tables-booking-app"
 	"tables-booking-app/pkg/handler"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	handlers := new(handler.Handler)
 
 	srv := new(tblBkng.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
